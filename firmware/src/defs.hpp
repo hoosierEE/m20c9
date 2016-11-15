8e076250bd1b81be823fcfcb07c6ec3cdc7fe391
hoosieree <ashroyer@gmail.com>
2016-11-15 08:40:36 -0500

    defs.hpp

---
+++ firmware/src/defs.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <array>
+
+using std::array;
+
+const uint8_t cols{5},rows{4},caps{9};
+
+/* Pins */
+const array<uint8_t,caps>tnsy_cap_pins{0,1,15,16,17,18,19,22,23};
+const array<uint8_t,cols>tnsy_col_pins{2,5,6,7,8};
+const array<uint8_t,rows>tnsy_row_pins{10,11,12,14};
+
+typedef const array<array<uint8_t,cols>,rows> MechLayer;
+typedef const uint8_t CapKey;
+
+/* TODO Capacitive Keys */
+CapKey space{15};
+
+/* Mechanical Keys */
+
+/* Left Hand */
+MechLayer Ldefault{{
+        {'1', '2', '3', '4', '5'},
+        {'q', 'w', 'e', 'r', 't'},
+        {'a', 's', 'd', 'f', 'g'},
+        {'z', 'x', 'c', 'v', 'b'}}};
+
+MechLayer Lshifted{{
+        {'!', '@', '#', '$', '%'},
+        {'Q', 'W', 'E', 'R', 'T'},
+        {'A', 'S', 'D', 'F', 'G'},
+        {'Z', 'X', 'C', 'V', 'B'}}};
+
+MechLayer Lsymbols{{
+        {' ', ' ', ' ', ' ', ' '},
+        {'@', '#', '$', '%', '['},
+        {' ', '-', '+', '=', '('},
+        {'_', '~', '`', '!', '{'}}};
+
+/* Right Hand */
+MechLayer Rdefault{{
+        {'6', '7', '8', '9', '0'},
+        {'y', 'u', 'i', 'o', 'p'},
+        {'h', 'j', 'k', 'l', '\''},
+        {'n', 'm', ',', '.', '-'}}};
+
+MechLayer Rshifted{{
+        {'^', '&', '*', '(', ')'},
+        {'Y', 'U', 'I', 'O', 'P'},
+        {'H', 'J', 'K', 'L', '"'},
+        {'N', 'M', '?', '!', ':'}}};
+
+MechLayer Rsymbols{{
+        {' ', ' ', ' ', ' ', ' '},
+        {']', '^', '&', '*', '|'},
+        {')', ';', ':', '/', '\\'},
+        {'}', '<', ',', '.', '>'}}};
+
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,59 +1,13 @@
 /* m30c9 -- keyboard firmware for Teensy 3.0
    https://github.com/hoosierEE/m30c9 */
 #include <Arduino.h>
-#include <array>
+#include "defs.hpp"
 
 /* HID keyboard data rate limited to 1ms. */
 const uint32_t rate{10};/* 10ms */
 elapsedMillis t;
 
-using std::array;
-const uint8_t cols{5},rows{4},caps{9};
-const array<uint8_t,caps>tnsy_cap_pins{0,1,15,16,17,18,19,22,23};
-const array<uint8_t,cols>tnsy_col_pins{2,5,6,7,8};
-const array<uint8_t,rows>tnsy_row_pins{10,11,12,14};
-typedef const array<array<uint8_t,cols>,rows> MechLayer;
 
-/* Capacitive Keys */
-
-
-/* Left Hand */
-MechLayer Ldefault{{
-        {'1', '2', '3', '4', '5'},
-        {'q', 'w', 'e', 'r', 't'},
-        {'a', 's', 'd', 'f', 'g'},
-        {'z', 'x', 'c', 'v', 'b'}}};
-
-MechLayer Lshifted{{
-        {'!', '@', '#', '$', '%'},
-        {'Q', 'W', 'E', 'R', 'T'},
-        {'A', 'S', 'D', 'F', 'G'},
-        {'Z', 'X', 'C', 'V', 'B'}}};
-
-MechLayer Lsymbols{{
-        {' ', ' ', ' ', ' ', ' '},
-        {'@', '#', '$', '%', '['},
-        {' ', '-', '+', '=', '('},
-        {'_', '~', '`', '!', '{'}}};
-
-/* Right Hand */
-MechLayer Rdefault{{
-        {'6', '7', '8', '9', '0'},
-        {'y', 'u', 'i', 'o', 'p'},
-        {'h', 'j', 'k', 'l', '\''},
-        {'n', 'm', ',', '.', '-'}}};
-
-MechLayer Rshifted{{
-        {'^', '&', '*', '(', ')'},
-        {'Y', 'U', 'I', 'O', 'P'},
-        {'H', 'J', 'K', 'L', '"'},
-        {'N', 'M', '?', '!', ':'}}};
-
-MechLayer Rsymbols{{
-        {' ', ' ', ' ', ' ', ' '},
-        {']', '^', '&', '*', '|'},
-        {')', ';', ':', '/', '\\'},
-        {'}', '<', ',', '.', '>'}}};
 
 /* Both: scan own switches, send/receive messages from other half.
    1 half: commuincate with host over BTLE.
