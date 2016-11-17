39591f7b8d77ec00f2a924d5fcccd5296b6e2f34
Alex <ashroyer@indiana.edu>
2016-11-17 17:06:36 -0500

    simpler

--- keyboard/src/keymap.hpp
+++ keyboard/src/keymap.hpp
@@ -1,51 +1,17 @@
-// Left side mechanical keymap
-#include "m20c9.hpp"
+// Capacitive keys
+// alt
+// ctrl
+// gui
+// space
 
+// Special keys -- each enables a different MechLayer
+// movement
+// shift
+// editor
+// fn
 #ifdef m20c9_LEFT
-MechLayer dflt{{
-        {'1', '2', '3', '4', '5'},
-        {'q', 'w', 'e', 'r', 't'},
-        {'a', 's', 'd', 'f', 'g'},
-        {'z', 'x', 'c', 'v', 'b'}}};
-
-MechLayer shft{{
-        {'!', '@', '#', '$', '%'},
-        {'Q', 'W', 'E', 'R', 'T'},
-        {'A', 'S', 'D', 'F', 'G'},
-        {'Z', 'X', 'C', 'V', 'B'}}};
-
-MechLayer syms{{
-        {' ', ' ', ' ', ' ', ' '},
-        {'@', '#', '$', '%', '['},
-        {' ', '-', '+', '=', '('},
-        {'_', '~', '`', '!', '{'}}};
-// TODO: Left side capacitive keymap
-
+#include "keymap_left.hpp"
 #else
-
-// Right side mechanical keymap
-MechLayer dflt{{
-        {'6', '7', '8', '9', '0'},
-        {'y', 'u', 'i', 'o', 'p'},
-        {'h', 'j', 'k', 'l', '\''},
-        {'n', 'm', ',', '.', '-'}}};
-
-MechLayer shft{{
-        {'^', '&', '*', '(', ')'},
-        {'Y', 'U', 'I', 'O', 'P'},
-        {'H', 'J', 'K', 'L', '"'},
-        {'N', 'M', '?', '!', ':'}}};
-
-MechLayer syms{{
-        {' ', ' ', ' ', ' ', ' '},
-        {']', '^', '&', '*', '|'},
-        {')', ';', ':', '/', '\\'},
-        {'}', '<', ',', '.', '>'}}};
-// TODO: Right side capacitive keymap
+#include "keymap_right.hpp"
 #endif
 
-MechLayer MLediting{{
-        {MODIFIERKEY_CTRL,MODIFIERKEY_SHIFT,MODIFIERKEY_ALT,MODIFIERKEY_GUI},
-        {MODIFIERKEY_CTRL,MODIFIERKEY_SHIFT,MODIFIERKEY_ALT,MODIFIERKEY_GUI},
-        {MODIFIERKEY_CTRL,MODIFIERKEY_SHIFT,MODIFIERKEY_ALT,MODIFIERKEY_GUI},
-        {MODIFIERKEY_CTRL,MODIFIERKEY_SHIFT,MODIFIERKEY_ALT,MODIFIERKEY_GUI}}};
--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "m20c9.hpp"
+#include "readers.hpp"
 #include "keymap.hpp"
 
 CapsRaw cr;
@@ -7,11 +7,26 @@ CapsTouched ct;
 MechsPressed mp;
 
 void setup(){
-    setup_matrix();
+    for(const auto&i : tnsy_col_pins) pinMode(i,INPUT_PULLUP);
+    for(const auto&i : tnsy_row_pins) pinMode(i,OUTPUT_OPENDRAIN);
 }
 
 void loop(){
-    read_mech(mp);
-    read_caps_raw(cr);
-    read_caps(cr,ct);
+
+    // Read mechanical switches
+    for(uint8_t row=0; row<rows; ++row){
+        digitalWrite(tnsy_row_pins[row],LOW);
+        for(uint8_t col=0; col<cols; ++col){
+            mp[row][col] = digitalRead(tnsy_col_pins[col]);
+        }
+        digitalWrite(tnsy_row_pins[row],HIGH);
+    }
+
+    // Read capacitive pads
+    for(uint8_t i=0;i<caps;++i){
+        cr[i] = touchRead(tnsy_cap_pins[i]);
+        // TODO determine if a more sophisticated threshold is needed
+        ct[i] = 1000 < cr[i];
+    }
+
 }
